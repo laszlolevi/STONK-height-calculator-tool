12bf8d09fbe89241dc21e4ebc39099235490116e
GKS2MC <GKS2MC@MC8N0209>
2020-09-04 10:47:46 +0200

    stonk-height-calculator-tool

---
+++ HeightCalculatorTool/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeightCalculatorTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "Your height is "+textBox1.Text.ToString()+"cm";
+            string title = "Result";
+            MessageBoxButtons button = MessageBoxButtons.OK;
+            MessageBox.Show(message, title,button,MessageBoxIcon.Information);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+    }
+}
